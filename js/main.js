3ddbdae195c1fc280f5a2a63ac87fd4a073f546a
Rocky Liwanag <studiorockprod@me.com>
2020-02-09 18:39:20 -0800

    Added background image and buttons, styled html elements

--- js/main.js
+++ js/main.js
@@ -1,37 +1,51 @@
 //Blackjack Psuedocode
 
 //Constants needed
-//Winning number - 21
-//Deck of cards A to K
+//Winning hand = 21
+//Array Deck of cards A to K plus suits
+
 
 //Other Variables
 //Dealer hand
 //User hand
 //player Total
 //Dealer Total
+//Shuffle Cards
+//player turn
+
+//cached elements
+//deck of cards
+//player/computer sum
+
+
+//event listeners
 //Hit Button
 //Stay Button
 //Deal Button
-//Shuffle Cards
 
 
 
-//Game initializes, cards need to be shuffled
-//Cards get dealt randomly
-//Player and Dealer get dealt 2 cards
-//Get sum of player cards on hand
-//Get sum of dealer cards on hand
-//Dealer logic
-    //if sum of cards are less than 17 dealer hits
-    //if sum of cards are greater than or equal to 17 dealer hits
-    //if sum of cards is a soft 17 dealer hits
-    //if sum of cards are greater than 21 dealer busts
+//-Game initializes, cards need to be shuffled randomnly
+//-Cards get dealt 2 cards to dealer/player/dealer/player
+//-Get sum of player cards on hand
+//-Get sum of dealer cards on hand
+
+//Dealer move logic
+    //-if sum of cards are less than 17 dealer hits
+    //-if sum of cards are greater than or equal to 17 dealer 
+    //  stays
+    //-if sum of cards is a soft 17 dealer hits //throw an or
+    //-if sum of cards are greater than 21 dealer busts
 
 //Win logic
-//if player sum of cards is greater than 21 player busts, dealer wins
-//if dealer sum of cards is greater than 21 and player is less than 21, player wins
-//if player sum of cards is greater than 21 and dealer is less than 21, dealer wins
-//if player and dealer is less than 21 and hold cards, whoever is closest to 21 wins
+//-if player sum of cards is greater than 21 player busts, 
+//  dealer wins
+//-if dealer sum of cards is greater than 21 and player is 
+//  less than 21, player wins
+//-if player sum of cards is greater than 21 and dealer is 
+//  less than 21, dealer wins
+//-if player and dealer is less than 21 and hold cards, 
+//  whoever is closest to 21 wins
 
 
 
